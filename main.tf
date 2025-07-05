8c9fc75c6c174538e9bbe5e6263da531abbffbe2
TK0911 <singhtanveer@hotmail.com>
2025-07-05 14:17:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default  = true
+}
+
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -21,4 +27,24 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "Learning Terraform"
   }
+}
+
+resource "aws_security_group" "blog" {
+name = "blog"
+description = "Allow http and https in. Allow everything out"
+
+vpc_id = data.aws_vpc.default.id
+
+}
+
+resources "aws_security_group_rule" "blog_http_in" {
+type = "ingress"
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0./0"]
+
+security_group_id = aws_security_
+
+
 }

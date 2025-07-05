4951d75e604fb660fba3f98fc02522d0e28773d7
TK0911 <singhtanveer@hotmail.com>
2025-07-05 14:31:15 +0100

    create security group

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "Learning Terraform"
   }
@@ -37,14 +39,32 @@ vpc_id = data.aws_vpc.default.id
 
 }
 
-resources "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
 type = "ingress"
 from_port = 80
 to_port = 80
 protocol = "tcp"
 cidr_blocks = ["0.0.0./0"]
 
-security_group_id = aws_security_
+security_group_id = aws_security_group.blog.id
+}
 
+resource "aws_security_group_rule" "blog_https_in" {
+type = "ingress"
+from_port = 443
+to_port = 443
+protocol = "tcp"
+cidr_blocks = ["0.0.0./0"]
 
+security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+type = "egress"
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0./0"]
+
+security_group_id = aws_security_group.blog.id
 }

783fce6cfed12833ae09636d5b03846c4416dfba
TK0911 <singhtanveer@hotmail.com>
2025-07-09 13:13:42 +0100

    Error corrected

--- main.tf
+++ main.tf
@@ -34,8 +34,7 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-name = "blog
-
+name = "blog"
 vpc_id = data.aws_vpc.default.id
 
 ingress_rules = ["http-80-tcp","https-443-tcp"]
